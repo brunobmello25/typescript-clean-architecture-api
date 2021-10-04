8873722762f90b767d88df1b4d8c42d7d6e0bf96
Bruno Mello <bruno.barros.mello@gmail.com>
2021-10-04 20:41:35 -0300

    test: ensure BCryptAdapter returns a hash on success

--- src/infra/encrypter/bcrypt-adapter.spec.ts
+++ src/infra/encrypter/bcrypt-adapter.spec.ts
@@ -11,4 +11,18 @@ describe('BCrypt Adapter', () => {
 
     expect(spy).toHaveBeenCalledWith('any_value', 12);
   });
+
+  test('should return a hash on success', async () => {
+    const sut = new BcryptAdapter(12);
+
+    const hash = await sut.encrypt('any_value');
+
+    expect(hash).toBe('any_hash');
+  });
 });
+
+jest.mock('bcrypt', () => ({
+  async hash(_value: string, _salt: number): Promise<string> {
+    return 'any_hash';
+  },
+}));
