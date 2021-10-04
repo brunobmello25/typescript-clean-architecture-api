487dd51dfe5a7e61646bb661ceff53242f4766eb
Bruno Mello <bruno.barros.mello@gmail.com>
2021-10-04 19:49:43 -0300

    refactor: move sut creation to a helper factory

--- src/data/usecases/create-user/db-create-user.spec.ts
+++ src/data/usecases/create-user/db-create-user.spec.ts
@@ -3,13 +3,7 @@ import { DbCreateUser } from './db-create-user';
 
 describe('DBCreateUser UseCase', () => {
   test('should call Encrypter with proper password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt(_value: string): Promise<string> {
-        return 'hashed_password';
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbCreateUser(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
 
     await sut.create({ name: 'valid_name', email: 'valid_email@email.com', password: 'valid_password' });
@@ -17,3 +11,25 @@ describe('DBCreateUser UseCase', () => {
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
   });
 });
+
+interface SutTypes {
+  sut: DbCreateUser;
+  encrypterStub: Encrypter;
+}
+
+function makeSut(): SutTypes {
+  class EncrypterStub implements Encrypter {
+    async encrypt(_value: string): Promise<string> {
+      return 'hashed_password';
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+
+  const sut = new DbCreateUser(encrypterStub);
+
+  return {
+    sut,
+    encrypterStub,
+  };
+}
