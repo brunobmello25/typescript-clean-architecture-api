40d12d9953133d8ca552a94447812a0a6469db02
Bruno Mello <bruno.barros.mello@gmail.com>
2021-10-19 19:34:15 -0300

    refactor: add middleware index file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,6 @@
 import { Express } from 'express';
 
-import { bodyParser } from '../middlewares/body-parser';
-import { cors } from '../middlewares/cors';
-import { contentType } from '../middlewares/content-type';
+import { bodyParser, contentType, cors } from '../middlewares';
 
 export function configMiddlewares(app: Express) {
   app.use(bodyParser);
