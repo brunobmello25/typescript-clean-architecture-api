7231910edf41cb9dc284e7a06b4074a72bad384e
Bruno Mello <bruno.barros.mello@gmail.com>
2021-10-19 19:32:30 -0300

    feat: ensure app uses proper content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -2,8 +2,10 @@ import { Express } from 'express';
 
 import { bodyParser } from '../middlewares/body-parser';
 import { cors } from '../middlewares/cors';
+import { contentType } from '../middlewares/content-type';
 
 export function configMiddlewares(app: Express) {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 }
